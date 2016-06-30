c0709e10fea70b70f24192c72a24b468fa644255
Joey <joeymejias@gmail.com>
2016-06-30 14:15:02 -0400

    lab complete

--- starter-code/app/src/main/java/ga/demo/viewslab/MainActivity.java
+++ starter-code/app/src/main/java/ga/demo/viewslab/MainActivity.java
@@ -2,15 +2,144 @@ package ga.demo.viewslab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.view.View.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    // ListView listView;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Put your Java code in here
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Toast.makeText(MainActivity.this, "You clicked button 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Toast.makeText(MainActivity.this, "You clicked button 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Toast.makeText(MainActivity.this, "You clicked button 3", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Toast.makeText(MainActivity.this, "You clicked button 4", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final Button button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Toast.makeText(MainActivity.this, "You clicked button 5", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final Button button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Toast.makeText(MainActivity.this, "You clicked button 6", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final Button button7 = (Button) findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Toast.makeText(MainActivity.this, "You clicked button 7", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final Button button8 = (Button) findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Toast.makeText(MainActivity.this, "You clicked button 8", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final Button button9 = (Button) findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Toast.makeText(MainActivity.this, "You clicked button 9", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final Button button10 = (Button) findViewById(R.id.button10);
+        button10.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Toast.makeText(MainActivity.this, "You clicked button 10", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        View.OnClickListener clickListener = new View.OnClickListener() {
+//            public void onClick(View view) {
+//                Toast.makeText(view.getContext(), "Button clicked!", Toast.LENGTH_SHORT).show();
+//            }
+//        };
+//
+//        mButton.setOnClickListener(clickListener);
+
+//        // Put your Java code in here
+//        // Get ListView object from xml
+//        listView = (ListView) findViewById(R.id.listView);
+//
+//        mButton = (Button) findViewById(R.id.button1);
+//        mButton.setVisibility(INVISIBLE);
+//
+//        // Defined Array values to show in ListView
+//        String[] values = new String[] { "This is TextView number 1" + "\n" + "This is TextView number 2",
+//                "This is TextView number 3" + "\n" + "This is TextView number 4",
+//                "This is TextView number 5" + "\n" + "This is TextView number 6",
+//                "This is TextView number 7" + "\n" + "This is TextView number 8",
+//                "This is TextView number 9" + "\n" + "This is TextView number 10",
+//                "This is TextView number 11" + "\n" + "This is TextView number 12",
+//                "This is TextView number 13" + "\n" + "This is TextView number 14",
+//                "This is TextView number 15" + "\n" + "This is TextView number 16",
+//                "This is TextView number 17" + "\n" + "This is TextView number 18",
+//                "This is TextView number 19" + "\n" + "This is TextView number 20"
+//        };
+//
+//        // Define a new Adapter
+//        // First parameter - Context
+//        // Second parameter - Layout for the row
+//        // Third parameter - ID of the TextView to which the data is written
+//        // Forth - the Array of data
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+//                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+//
+//        // Assign adapter to ListView
+//        listView.setAdapter(adapter);
 
     }
 }
